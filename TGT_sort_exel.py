1dde5de6f8d2c1462884b2ad33c1f41833ba6375
ZakievI <143954875+ZakievI@users.noreply.github.com>
2024-06-04 15:59:05 +0300

    04.06.24

--- TGT_sort_exel.py
+++ TGT_sort_exel.py
@@ -1,5 +1,6 @@
 import openpyxl as opxl
 import numpy as np
+import time 
 
 def month_to_number(month):
     months = {
@@ -22,42 +23,57 @@ def month_to_number(month):
 def sort_exels(file,file_new):
     book=opxl.open(file,read_only=True)
     sheet=book.active
-    data=[]
-    info=[sheet[4][8].value,sheet[5][8].value,sheet[6][8].value,sheet[7][8].value, sheet[8][8].value,sheet[9][8].value,sheet[10][8].value,sheet[11][8].value,sheet[12][8].value,sheet[13][8].value,sheet[14][8].value,sheet[15][8].value,sheet[16][8].value,sheet[17][8].value,sheet[18][8].value]
-    j=5
-    while (sheet[j][1].value!=None):
-        month_y=sheet[j][1].value
+    data=[] # создаем массив для хранения данных за весь промежуток времени
+    if(sheet.cell(row=5,column=2).value!=None):
+        info=[sheet.cell(row=4,column=9).value]
+        size_info=1 #количество параметров
+        while(sheet.cell(row=4,column=9).value!=sheet.cell(row=4+size_info,column=9).value):
+            info.append(sheet.cell(row=4+size_info,column=9).value)
+            size_info+=1
+    print("Start project:")
+    start_time=time.time()
+    start_=5
+    while (sheet.cell(row=start_,column=2).value!=None):
+        time_=time.time()
+        month_y=sheet.cell(row=start_,column=2).value
         size=len(month_y)
         month=month_to_number(month_y[0:size-6])
         year=month_y[size-4:]
-        date=[] # создаем пустой массив 
-        i=9
-        while(sheet[3][i].value!=None):
-            if(sheet[j-1][i].value==None):
+        date_m=[] # создаем массив для хранения данных для определенного месяца
+        i=10
+        while(sheet.cell(row=start_-1,column=i).value!=None):
+            if(sheet.cell(row=start_-1,column=i).value==None):
                 i=i+1
                 continue
-            date.append(list([(str(sheet[3][i].value)+month+"."+year), sheet[j-1][i].value, sheet[j][i].value, sheet[j+1][i].value, sheet[j+2][i].value, sheet[j+3][i].value, sheet[j+4][i].value, sheet[j+5][i].value, sheet[j+6][i].value, sheet[j+7][i].value, sheet[j+8][i].value, sheet[j+9][i].value, sheet[j+10][i].value, sheet[j+11][i].value, sheet[j+12][i].value, sheet[j+13][i].value])) # получаем данные 
+            date_d=[] # создаем массив для хранения данных для определенного дня
+            date_d.append(str(sheet.cell(row=3,column=i).value)+month+"."+year)
+            for k in range(0, size_info):
+                date_d.append(sheet.cell(row=start_-1+k,column=i).value)
+            date_m.append(date_d)
             i=i+1
-        data.append(list(date))
-        print(j)
-        j=j+15
+        data.append(date_m)
+        print("Current line:= "+str(start_)+", execution speed: "+str(time.time()-time_))
+        start_+=size_info
 
     data.reverse()
     book_new=opxl.Workbook()
     sheet_new=book_new.active
 
-    for i in range(0,15):
+    for i in range(0,size_info):
         sheet_new.cell(row=1,column=2+i).value=info[i]
 
     i=0
-    for data_new in data:
-        for date_new in data_new:
-            for j in range(0,15):
-                sheet_new.cell(row=2+i,column=1+j).value=date_new[j]
+    for date_m_ in data:
+        for date_d_ in date_m_:
+            for j in range(0,size_info+1):
+                sheet_new.cell(row=2+i,column=1+j).value=date_d_[j]
             i=i+1
     book_new.save(file_new)
     book_new.close()
     book.close()
+    print("Finish project, all time:"+str(time.time()-start_time))
 
 
-sort_exels("Скв. 388 (01.11.2016-30.11.2023).xlsx","Скв. 388 (01.11.2016-30.11.2023)_new.xlsx")
+sort_exels("Скв. 9543 (01.02.2022-30.06.2023)_Газпромнефть 1.xlsx","Скв. 9543 (01.02.2022-30.06.2023)_Газпромнефть 1_new.xlsx")
+# sort_exels("Скв. 388 (01.11.2016-30.11.2023).xlsx","1_new.xlsx")
+
